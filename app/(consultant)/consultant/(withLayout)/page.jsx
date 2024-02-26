1117f694efbb986ba9e35c873754c0da10f29df3
la3jiz <lazizamin8@gmail.com>
2024-02-26 23:19:26 +0100

    mission details not completed

--- app/(consultant)/consultant/(withLayout)/page.jsx
+++ app/(consultant)/consultant/(withLayout)/page.jsx
@@ -15,7 +15,6 @@ import MissionChart from "@/components/partials/chart/consultant/mission-chart";
 import AddNewMission from "@/components/ui/modals/pages/consultant-home/addNewMission";
 import Dropdown from "@/components/ui/Dropdown";
 import Link from "next/link";
-import { Menu } from "@headlessui/react";
 import AddCRA from "@/components/ui/modals/pages/consultant-home/addCRA";
 import UpdateTJM from "@/components/ui/modals/pages/consultant-home/updateTJM";
 
@@ -145,6 +144,41 @@ const COLUMNS = [
         <span className="text-slate-600 dark:text-slate-400">
           {row?.cell?.value}
         </span>
+      );
+    },
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+    Cell: (row) => {
+      return (
+        <div className=" text-center">
+          <Dropdown
+            classMenuItems="right-0 w-[140px] bottom-[40%] z-1000  "
+            label={
+              <span className="text-xl text-center block w-full">
+                <Icon icon="heroicons-outline:dots-vertical" />
+              </span>
+            }
+          >
+            <div className="divide-y divide-slate-100 dark:divide-slate-800">
+              <Link
+                href={`/consultant/mission/${row?.cell?.row?.original?.id}`}
+              >
+                <div
+                  className={`${"hover:bg-slate-900 hover:text-white dark:hover:bg-slate-600 dark:hover:bg-opacity-50"}
+                    w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm  last:mb-0 cursor-pointer
+                    first:rounded-t last:rounded-b flex  space-x-2 items-center rtl:space-x-reverse `}
+                >
+                  <span className="text-base">
+                    <Icon icon="heroicons:list-bullet" width={15} />
+                  </span>
+                  <span>Détails</span>
+                </div>
+              </Link>
+            </div>
+          </Dropdown>
+        </div>
       );
     },
   },
@@ -304,6 +338,20 @@ const ConsultantDashboard = () => {
                 }
               >
                 <div className="divide-y divide-slate-100 dark:divide-slate-800">
+                  <Link
+                    href={`/consultant/mission/${row?.cell?.row?.original?.id}`}
+                  >
+                    <div
+                      className={`${"hover:bg-slate-900 hover:text-white dark:hover:bg-slate-600 dark:hover:bg-opacity-50"}
+                      w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm  last:mb-0 cursor-pointer
+                      first:rounded-t last:rounded-b flex  space-x-2 items-center rtl:space-x-reverse `}
+                    >
+                      <span className="text-base">
+                        <Icon icon="heroicons:list-bullet" width={15} />
+                      </span>
+                      <span>Détails</span>
+                    </div>
+                  </Link>
                   <AddCRA
                     data={row?.cell?.row?.original}
                     toggleDropdown={toggleDropdown}
